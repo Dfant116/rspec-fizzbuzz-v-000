9f02c0279010d7e6d451c48c5190a51d7ffb48bf
Dalton Fant <daltonfant16@gmail.com>
2018-01-03 22:45:42 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,10 +1,9 @@
 def fizzbuzz(x)
   if x % 3 == 0
-    puts "Fizz"
+    "Fizz"
   elsif x % 5 == 0
-    puts "Buzz"
+    "Buzz"
   else x % 15 == 0
-    puts "FizzBuzz"
+    "FizzBuzz"
   end
 end
-
